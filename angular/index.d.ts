06c454985b28ab2576e2e7db9eab6ccf32658857
Dima Grossman <dima@grossman.io>
2016-12-16 01:28:41 +0200

    quick fix

--- angular/index.d.ts
+++ angular/index.d.ts
@@ -14,8 +14,8 @@ declare global {
     }
 }
 
-export as namespace angular;
-export as namespace ng;
+//export as namespace angular;
+//export as namespace ng;
 
 // Support AMD require
 export = angular;
